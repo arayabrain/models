606489ee99ec25bfb89de202f31ba7e75169138f
iiou16 <acc12035vp@es2.abci.local>
2020-07-21 09:56:42 +0900

    [update] update train parameter

--- official/vision/image_classification/mobilenet_v1/mobilenet_v1_config.py
+++ official/vision/image_classification/mobilenet_v1/mobilenet_v1_config.py
@@ -25,14 +25,6 @@ import dataclasses
 from official.vision.image_classification.configs import base_configs
 
 
-_MBNETV1_LR_SCHEDULE = [    # (multiplier, epoch to start) tuples
-    (1.0, 5), (0.1, 30), (0.01, 60), (0.001, 80)
-]
-_MBNETV1_LR_BOUNDARIES = list(p[1] for p in _MBNETV1_LR_SCHEDULE[1:])
-_MBNETV1_LR_MULTIPLIERS = list(p[0] for p in _MBNETV1_LR_SCHEDULE)
-_MBNETV1_LR_WARMUP_EPOCHS = _MBNETV1_LR_SCHEDULE[0][1]
-
-
 @dataclasses.dataclass
 class MobileNetV1ModelConfig(base_configs.ModelConfig):
   """Configuration for the MobileNetV1 model."""
@@ -41,21 +33,17 @@ class MobileNetV1ModelConfig(base_configs.ModelConfig):
   model_params: Mapping[str, Any] = dataclasses.field(default_factory=lambda: {
       'num_classes': 1000,
       'batch_size': None,
-      # 'use_l2_regularizer': True,
-      # 'rescale_inputs': False,
   })
   loss: base_configs.LossConfig = base_configs.LossConfig(
       name='sparse_categorical_crossentropy')
   optimizer: base_configs.OptimizerConfig = base_configs.OptimizerConfig(
-      name='momentum',
+      name='rmsprop',
       decay=0.9,
       epsilon=0.001,
       momentum=0.9,
       moving_average_decay=None)
-  learning_rate: base_configs.LearningRateConfig = (
-      base_configs.LearningRateConfig(
-          name='piecewise_constant_with_warmup',
-          examples_per_epoch=1281167,
-          warmup_epochs=_MBNETV1_LR_WARMUP_EPOCHS,
-          boundaries=_MBNETV1_LR_BOUNDARIES,
-          multipliers=_MBNETV1_LR_MULTIPLIERS))
+  learning_rate: base_configs.LearningRateConfig = base_configs.LearningRateConfig(  # pylint: disable=line-too-long
+      name='exponential',
+      initial_lr=0.045,
+      decay_epochs=2.5,
+      decay_rate=0.94)
