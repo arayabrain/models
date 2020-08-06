c5e7a3429a1fcd97e5739da9f81c8efc3fed3b85
nobutoba <otoba@araya.org>
2020-08-06 12:14:57 +0900

    Handle functools.partial

--- official/vision/image_classification/callbacks.py
+++ official/vision/image_classification/callbacks.py
@@ -107,11 +107,10 @@ class CustomTensorBoard(tf.keras.callbacks.TensorBoard):
     lr_schedule = getattr(self.model.optimizer, "lr", None)
     if isinstance(lr_schedule, tf.keras.optimizers.schedules.LearningRateSchedule):
         logs["learning_rate"] = tf.keras.backend.get_value(
-            lr_schedule(
-                tf.keras.backend.get_value(
-                    self.model.optimizer.iterations)
-            )
+            lr_schedule(self.model.optimizer.iterations)
         )
+        if isinstance(logs["learning_rate"], functools.partial):
+          logs["learning_rate"] = logs["learning_rate"]()
     return logs
 
   def _log_metrics(self, logs, prefix, step):
