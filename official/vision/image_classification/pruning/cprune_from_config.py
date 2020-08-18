535669c194caadca771a6a677c4ad21e07be7805
nobutoba <otoba@araya.org>
2020-08-18 13:23:58 +0900

    Debug

--- official/vision/image_classification/pruning/cprune_from_config.py
+++ official/vision/image_classification/pruning/cprune_from_config.py
@@ -738,7 +738,9 @@ def prune_physically(model):
       if prev_layer_name is not None:
         prev_layer = model.get_layer(prev_layer_name)
         assert prev_layer.bias is None
-        chin_indices = _get_nonvanishing_channels(prev_layer.kernel, ch_axis=chout_axis)
+        prev_layer_kernel = prev_layer.depthwise_kernel if type(layer) is tf.keras.layers.DepthwiseConv2D \
+            else prev_layer.kernel
+        chin_indices = _get_nonvanishing_channels(prev_layer_kernel, ch_axis=chout_axis)
 
         if type(prev_layer) is tf.keras.layers.Conv2D:
           if type(layer) is tf.keras.layers.Dense:
